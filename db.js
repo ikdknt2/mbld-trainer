e061fb82acd91e71d851f26661f5447357055ef3
ikdknt2 <ikdknt2@gmail.com>
2026-03-06 16:48:06 +0900

    add date

--- db.js
+++ db.js
@@ -18,24 +18,28 @@ function saveTime(){
     const solved = parseInt(document.getElementById("solvedInput").value);
     const attempted = parseInt(document.getElementById("attemptedInput").value);
 
+    const point = solved - (attempted - solved);
+
+    const now = new Date();
+
     const tx = db.transaction("times","readwrite");
     const store = tx.objectStore("times");
 
     store.add({
         time:time,
         solved:solved,
-        attempted:attempted
+        attempted:attempted,
+        point:point,
+        date:now.toISOString()
     });
 
     tx.oncomplete = function(){
 
         loadTimes();
 
-        // 入力欄クリア
-        document.getElementById("timeInput").value = "";
-        document.getElementById("solvedInput").value = "";
-        document.getElementById("attemptedInput").value = "";
-
+        document.getElementById("timeInput").value="";
+        document.getElementById("solvedInput").value="";
+        document.getElementById("attemptedInput").value="";
     };
 }
 
@@ -56,14 +60,38 @@ function loadTimes(){
 
             out += `
             <div>
-            ${data[i].solved} / ${data[i].attempted}
-            [${data[i].time}]
+            ${data[i].solved} / ${data[i].attempted} [${data[i].time}]
+            <button onclick="showInfo(${data[i].id})">...</button>
             <button onclick="deleteTime(${data[i].id})">❌</button>
             </div>
             `;
         }
 
         document.getElementById("timeList").innerHTML = out;
+    };
+}
+
+function showInfo(id){
+
+    const tx = db.transaction("times","readonly");
+    const store = tx.objectStore("times");
+
+    const req = store.get(id);
+
+    req.onsuccess = function(){
+
+        const d = req.result;
+
+        const date = new Date(d.date);
+
+        const formatted =
+        date.getFullYear() + "/" +
+        String(date.getMonth()+1).padStart(2,"0") + "/" +
+        String(date.getDate()).padStart(2,"0") + " " +
+        String(date.getHours()).padStart(2,"0") + ":" +
+        String(date.getMinutes()).padStart(2,"0");
+
+        alert(`${d.point} point, ${formatted}`);
     };
 }
 
@@ -83,3 +111,4 @@ function deleteTime(id){
 
 window.saveTime = saveTime;
 window.deleteTime = deleteTime;
+window.showInfo = showInfo;
